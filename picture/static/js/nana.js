d0b4af2eb99fa5b5699f1236c5eb7ed1ee32aece
sric <lzqiong@gmail.com>
2015-10-03 13:40:56 +0800

    add footer and go-top button

--- picture/static/js/nana.js
+++ picture/static/js/nana.js
@@ -74,6 +74,24 @@ function page_more(){
 	}
 	scrollListen();
 }
+$("#go-top").hide();
+//当滚动条的位置处于距顶部100像素以下时，跳转链接出现，否则消失
+$(function () {
+	$(window).scroll(function(){
+		if ($(window).scrollTop()>400){
+			$("#go-top").fadeIn(500);
+		}
+		else
+		{
+			$("#go-top").fadeOut(500);
+		}
+	});
+	//当点击跳转链接后，回到页面顶部位置
+	$("#go-top").click(function(){
+	$('body,html').animate({scrollTop:0},500);
+	return false;
+});
+});
 $(window).resize(resize_clip);
 $(document).ready( function() {
 	$(".album-entry").each(function(){
